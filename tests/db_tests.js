ac60a4a05b112d0637739c36a63aa03b1a473f22
Tamer Nasser <tamernasser@outlook.com>
2019-04-09 22:48:38 +0300

    Created test get reviews

--- tests/db_tests.js
+++ tests/db_tests.js
@@ -1,36 +1,36 @@
 const tape = require('tape');
-const runDbBuild = require('../database/db_build.sql');
-const getData = require('../src/queries/getPlaces.js');
-// const postData = require('../src/queries/postData.js');
+const runDbBuild = require('../database/db_build.js');
+const getData = require('../database/queries/readdata.js');
+const postData = require('../database/queries/postdata.js');
 
 tape('check that tape is working', (t) => {
   t.equals(1, 1, 'one equals one');
   t.end();
 })
 
-tape('testing getPlaces', (t) => {
+tape('testing get reviews', (t) => {
   runDbBuild((error, response) => {
-    t.error(err, "No errors in runDbBuild");
-    getData((error, data) => {
-      t.error(error, "No errors in getPlaces");
-      console.log(data);
-      t.deepEquals(data, [{id: 1, name: 'Tishreen', location: 'Nazareth'}]);
+    t.error(error, "runDbBuild successfully executed");
+    getData.getReviews((error, data) => {
+      t.error(error, "getReviews successfully executed");
+      // console.log(data);
+      t.deepEquals(data, [ { id: 1, place_id: 1, comment: 'A very great', rating: 3 }, { id: 2, place_id: 2, comment: 'Expensive place but good quality', rating: 3 } ]);
       t.end();
     })
   })
 })
 
-tape('testing postPlaces', (t) => {
-  runDbBuild((error, response) => {
-    t.error(err, "No errors in runDbBuild");
-    postData('Ibn Batuta Shawarma', 'Nazareth', (error, data) => {
-      t.error(error, "No errors in postPlaces");
-      getData((error, data) => {
-        t.error(error, "No errors in getPlaces");
-        console.log(data);
-        t.deepEquals(data, [{id: 1, name: 'Tishreen', location: 'Nazareth'}, {id: 2, name: 'Ibn Batuta Shawarma', location: "Nazareth"}]);
-        t.end();
-      })
-    })
-  })
-})
+// tape('testing postPlaces', (t) => {
+//   runDbBuild((error, response) => {
+//     t.error(err, "No errors in runDbBuild");
+//     postData('Ibn Batuta Shawarma', 'Nazareth', (error, data) => {
+//       t.error(error, "No errors in postPlaces");
+//       getData((error, data) => {
+//         t.error(error, "No errors in getPlaces");
+//         console.log(data);
+//         t.deepEquals(data, [{id: 1, name: 'Tishreen', location: 'Nazareth'}, {id: 2, name: 'Ibn Batuta Shawarma', location: "Nazareth"}]);
+//         t.end();
+//       })
+//     })
+//   })
+// })
