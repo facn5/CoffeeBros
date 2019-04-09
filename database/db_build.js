83657ded63dd8710f10a0b414400c62d37c416b1
Tamer Nasser <tamernasser@outlook.com>
2019-04-09 21:45:54 +0300

    Fixed SQL syntax error

--- database/db_build.js
+++ database/db_build.js
@@ -5,7 +5,6 @@ const dbConnection = require('./db_connection.js');
 
 const sqlPath = path.join(__dirname, 'db_build.sql');
 const sql = fs.readFileSync(sqlPath).toString();
-
 // dbConnection.query(sql, (err, res) => {
 //   if (err) throw err;
 //   console.log('Users table created with result: ', res);
